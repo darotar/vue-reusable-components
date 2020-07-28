f69afcbf1513a9979bdc789987ddd898248b3eef
aprok2 <anton.prokhorov@nike.com>
2020-07-28 15:50:53 +0200

    initialized Button and Icon

--- src/components/Button/Button.vue
+++ src/components/Button/Button.vue
@@ -3,17 +3,30 @@
     :is="computedTag"
     v-bind="$attrs"
     class="button"
-    v-on="$listeners"
     :class="[type, {
       'is-disabled': disabled,
       'is-rounded': rounded,
-      'is-sharp': sharp,
+      'is-active': active,
+      'is-primary': primary,
+      'is-secondary': secondary
     }]"
+    :style="buttonStyle"
+    v-on="!disabled && getListeners"
+    @mouseover="hover = true"
+    @mouseleave="hover = false"
   >
+    <n-icon v-if="iconLeft" :name="iconLeft" :size="getIconSize" />
+    <slot name="iconLeft" />
+
     <span v-if="label">{{ label }}</span>
-    <span v-else-if="$slots.default">
-      <slot />
-    </span>
+    <slot v-else-if="$slots.default" />
+
+    <n-icon
+      v-if="iconRight"
+      :name="iconRight"
+      :size="getIconSize"
+    />
+    <slot name="iconRight" />
   </component>
 </template>
 
@@ -25,23 +38,67 @@ export default {
   inheritAttrs: false,
 
   props: {
-    type: String,
-    size: String,
-    label: String,
-    iconLeft: String,
-    iconRight: String,
+    type: {
+      type: String,
+      default: '',
+    },
+    size: {
+      type: String,
+      default: '',
+    },
+    label: {
+      type: String,
+      default: '',
+    },
+    iconLeft: {
+      type: String,
+      default: '',
+    },
+    iconRight: {
+      type: String,
+      default: '',
+    },
+    iconSize: {
+      type: String,
+      default: '20',
+    },
+    width: {
+      type: String,
+      default: '',
+    },
+    height: {
+      type: String,
+      default: '',
+    },
+
+    onClick: {
+      type: Function,
+      default: () => {},
+    },
 
     loading: Boolean,
-    hovered: Boolean,
-    sharp: Boolean,
     rounded: Boolean,
     disabled: Boolean,
+    active: Boolean,
+    secondary: Boolean,
+    primary: Boolean,
 
     tag: {
       type: String,
       default: 'button',
       validator: value => config.defaultTagLinks.indexOf(value) >= 0,
-    }
+    },
+  },
+
+  data() {
+    return {
+      hover: false,
+      buttonStyle: {
+        width: `${this.width}px`,
+        height: `${this.height}px`,
+        'justify-content': this.iconLeft || this.iconRight ? 'space-between' : 'center',
+      },
+    };
   },
 
   computed: {
@@ -53,35 +110,93 @@ export default {
       }
 
       return this.tag;
-    }
-  }
-}
+    },
+
+    isHover() {
+      return !this.disabled ? (this.hover ? this.$colors.white : this.$colors.primaryBlack) : 'currentColor';
+    },
+
+    getIconSize() {
+      return this.type === 'is-action-text' ? '12' : this.iconSize;
+    },
+
+    getListeners() {
+      return { ...this.$listeners, click: this.$listeners.click || this.onClick };
+    },
+  },
+};
 </script>
 
 <style lang="scss">
+  .is-active {
+    background-color: var(--n-primary);
+    color: var(--n-white);
+    border-color: var(--n-primary);
+    font-weight: bold;
+  }
+
   .button {
+    font-family: HelveticaNeueRegular, sans-serif;
     display: inline-flex;
     white-space: nowrap;
     align-items: center;
-    justify-content: center;
     cursor: pointer;
     outline: none;
+    overflow: hidden;
+    user-select: none;
+    font-size: 10px;
   }
 
   .is-submit {
+    @extend .is-active;
+
     border-radius: 3px;
-    background-color: #000;
-    border: 1px solid #000;
-    color: #fff;
-    font-size: 14px;
+    color: var(--n-white);
     height: 40px;
     padding: 0 40px;
   }
 
+  .is-disabled {
+    box-shadow: none;
+    opacity: 0.4;
+    cursor: not-allowed;
+  }
+
+  .is-rounded {
+    border-radius: 20px;
+  }
+
+  .is-secondary {
+    border: 1px solid var(--n-primary);
+    color: var(--n-primary);
+    font-weight: bold;
+  }
+
+  .is-primary {
+    @extend .is-active;
+  }
+
   .is-action-tile {
-    border-radius: none;
-    border: 1px solid #e5e5e5;
+    border-radius: 0;
+    border: 1px solid var(--n-border);
     background: inherit;
+    color: var(--n-primary);
     text-transform: uppercase;
+    padding: 8px 11px 8px 16px;
+    font-weight: bold;
+    font-size: 10px;
+    flex: 0 0 195px;
+    height: 30px;
+    transition: var(--n-transition-common);
+
+    &:hover:not(.is-disabled) {
+      @extend .is-active;
+    }
+  }
+
+  .is-action-text {
+    color: var(--n-action-blue);
+    border-width: 0;
+    font-size: 12px;
   }
 </style>
--- src/components/Icon/Icon.vue
+++ src/components/Icon/Icon.vue
@@ -1,37 +1,69 @@
 <template>
-  <span class="icon" :class="[size]">
-     <i
-      v-if="!useIconComponent"
-      :class="[newPack, newIcon, newCustomSize, customClass]"
-    />
-    <component
-      v-else
-      :is="useIconComponent"
-      :icon="[newPack, newIcon]"
-      :size="newCustomSize"
-      :class="[customClass]"
-    />
-
-  </span>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    :width="size || width"
+    :height="size || height"
+    viewBox="0 0 64 64"
+    :aria-labelledby="name"
+    role="presentation"
+  >
+    <title
+      :id="name"
+      lang="en"
+    >{{ name }} icon</title>
+    <g :fill="iconColor">
+      <component :is="getIcon" />
+    </g>
+  </svg>
 </template>
 
 <script>
-import config from '@/utils/config';
+import get from 'lodash/get';
+
+import icons from '@/components/icons';
 
 export default {
-  name: 'NIcon',
-
   props: {
-    type: [String, Object],
-    size: String,
-    icon: String,
-    component: String,
+    name: {
+      type: String,
+      default: 'box',
+    },
+
+    width: {
+      type: [Number, String],
+      default: 24,
+    },
+
+    height: {
+      type: [Number, String],
+      default: 24,
+    },
+
+    iconColor: {
+      type: String,
+      default: 'currentColor',
+    },
+
+    size: {
+      type: [Number, String],
+      default: '24',
+    },
   },
 
   computed: {
-    useIconComponent() {
-      return this.component || config.defaultIconComponent
-    }
-  }
-}
+    getIcon() {
+      const nodeText = get(this.$slots.default, '[0].text', '').trim();
+
+      let iconComponentName;
+
+      if (nodeText && nodeText[0] === '$') {
+        iconComponentName = `${nodeText[1].toUpperCase()}${nodeText.slice(2)}`;
+      } else {
+        iconComponentName = `${this.name[0].toUpperCase()}${this.name.slice(1)}`;
+      }
+
+      return icons[`${iconComponentName}Icon`];
+    },
+  },
+};
 </script>
